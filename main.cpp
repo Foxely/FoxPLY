855127c49f2ade73e6a8abffeb30b4954e402415
smbss1 <softandsgame@gmail.com>
2021-05-16 05:41:34 +0200

    feat: test make

---
+++ main.cpp
@@ -0,0 +1,9 @@
+
+#include <iostream>
+#include "print.hpp"
+
+int main()
+{
+    print();
+    return 0;
+}
---
+++ prints/print.cpp
@@ -0,0 +1,8 @@
+
+#include <iostream>
+
+int print()
+{
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
